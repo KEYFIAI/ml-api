045ddf167db431a0757bbd6045cd1d7d7eacf5eb
rafaelaraujo-ezops <renan.leme@ezops.com.br>
2022-01-25 10:48:28 -0300

    fix paths

--- devops/scripts/change_permissions.sh
+++ devops/scripts/change_permissions.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-chmod +x400 .\getenvs.sh
-chmod +x400 .\trainer.sh
-chmod +x400 .\stop_server.sh
-chmod +x400 .\start.sh
+chmod +x400 ./getenvs.sh
+chmod +x400 ./trainer.sh
+chmod +x400 ./stop_server.sh
+chmod +x400 ./start.sh
