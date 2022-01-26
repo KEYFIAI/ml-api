73e70823c2748f76b67ecd94fc7ad74291e838c8
Iago Mussel <him_mussel@live.com>
2022-01-26 17:59:35 -0300

    add to path

--- devops/scripts/after_install.sh
+++ devops/scripts/after_install.sh
@@ -4,7 +4,8 @@ cd /home/ec2-user/ml-api
 
 #check if the npm is installed
 if ! [ -x "$(command -v npm)" ]; then
-  echo 'npm is not installed. Installing npm...' >&2
+  echo 'npm is not installed. Installing npm...' 
+  PATH="/home/ec2-user/.nvm/versions/node/v17.4.0/bin:$PATH"
 fi	
 
 /home/ec2-user/.nvm/versions/node/v17.4.0/bin/npm install
