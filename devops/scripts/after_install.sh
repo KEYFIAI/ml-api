5a3228984f9a7fe590d0b8b1f40aaf21963d40ea
Iago Mussel <him_mussel@live.com>
2022-01-26 17:46:36 -0300

    add npm to path

--- devops/scripts/after_install.sh
+++ devops/scripts/after_install.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/ml-api
-/home/ec2-user/.nvm/versions/node/v17.4.0/bin/npm install
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+npm install
