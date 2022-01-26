8ccf1cfb16b76312a0e7ab7b288f95bad6aa0505
Iago Mussel <him_mussel@live.com>
2022-01-26 17:48:44 -0300

    check if npm is installed

--- devops/scripts/after_install.sh
+++ devops/scripts/after_install.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/ml-api
+#check if the npm is installed
+if ! [ -x "$(command -v npm)" ]; then
+  echo 'npm is not installed. Installing npm...' >&2
+  sudo yum install -y npm
+fi	
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
