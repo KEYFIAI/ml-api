d87b61ef6da3b38e243f41bc4b67b8737a08978c
rafaelaraujo-ezops <rafael.araujo@ezops.com.br>
2022-01-27 11:17:49 -0300

    Update Paths

--- src/py/python.js
+++ src/py/python.js
@@ -4,11 +4,11 @@ function python(message, script) {
   return new Promise((rs, rj) => {
     const options = {
       mode: "text",
-      pythonPath: "python", // LINUX or MAX - python3 | Window - python
-      scriptPath: "./src/py/recommendation-engine",
+      pythonPath: "python3", // LINUX or MAX - python3 | Window - python
+      scriptPath: "../././recommendation-engine",
       args: [message],
     };
-    
+
     ps.PythonShell.run(script, options, (err, results) => {
       if (err) throw err;
       rs(results);
