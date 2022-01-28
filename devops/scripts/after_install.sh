0703861139e653dfd6db0ad9fd7b6dc0b46addb1
rafaelaraujo-ezops <rafael.araujo@ezops.com.br>
2022-01-28 09:03:20 -0300

    Fix API

--- devops/scripts/after_install.sh
+++ devops/scripts/after_install.sh
@@ -9,3 +9,17 @@ if ! [ -x "$(command -v npm)" ]; then
 fi	
 
 /home/ec2-user/.nvm/versions/node/v17.4.0/bin/npm install
+ 
+mkdir src/py/recommendation-engine
+cp -r ../recommendation/pre_process_matrix.py src/py/recommendation-engine
+cp -r ../recommendation/recommender_nn.py src/py/recommendation-engine
+cp -r ../recommendation/recommender_loss_function.py src/py/recommendation-engine
+cp -r ../recommendation/wallet_token.py src/py/recommendation-engine
+cp -r ../recommendation/tokens.json src/py/recommendation-engine
+cp -r ../recommendation/tokens.json src/py/recommendation-engine
+cp -r ../recommendation/memories src/py/recommendation-engine
+cp -r ../recommendation/deployment-data src/py/recommendation-engine
+cp -r ../recommendation/file_path.py src/py/recommendation-engine
+cp -r ../recommendation/.env ./
+
+ 
